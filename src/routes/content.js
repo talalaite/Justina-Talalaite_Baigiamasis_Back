20835b1562461a6e9107fec56504882c1c0532af
talalaite <justina.talalaite@gmail.com>
2021-07-20 14:25:42 +0300

    auth

--- src/routes/content.js
+++ src/routes/content.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const middleware = require("../middleware");
+const mysql = require("mysql2/promise");
+const { mysqlConfig } = require("../config");
 
 router.get("/", middleware.loggedIn, (req, res) => {
   res.send({ msg: `Hi - I am logged in as ${req.userData.email}` });
