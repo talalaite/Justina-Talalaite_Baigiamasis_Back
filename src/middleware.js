7513a984d1d4b8e97ae508fc7c82aa5f95932b5c
talalaite <justina.talalaite@gmail.com>
2021-07-24 01:23:17 +0300

    added joi to middleare

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,27 @@
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
 const { jwtSecret } = require("./config");
 
+const userSchema = Joi.object({
+  email: Joi.string().email().max(250).trim().lowercase().required(),
+  password: Joi.string().min(6).max(250),required()
+});
+
 module.exports = {
+  async isAuthDataCorrect(req, res, next) {
+    let userData;
+    try {
+      userData = await userSchema.validateAsync({
+        email: req.body.email,
+        password: req.body.password,
+      });
+      req.userData = userData;
+      return next();
+    } catch (e) {
+      return res.status(400).send({ error: "Incorrect data passed" });
+    }
+  },
   loggedIn: (req, res, next) => {
     try {
       const token = req.headers.authorization?.split(" ")[1];
